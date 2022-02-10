c41d93eb7cfefd8439f9dded6e2d181748d530ee
diegodequintal <63262264+diegodequintal@users.noreply.github.com>
2022-02-09 22:10:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name="Yolov5Model",
       author="Diego De Quintal",
       authoer_email="diego.dequintal@newtoms.com",
       url='https://github.com/diegodequintal/Yolov5',
-      packages=['yolov5', 'yolov5.model','yolov5.data','yolov5.utils','deep_sort']
+      packages=['yolov5', 'yolov5.models','yolov5.data','yolov5.utils','deep_sort']
 )
